cd4f677af06b709fa67de13f303333608f87ec43
Wagner de Andrade Perin <wagnerperin@gmail.com>
2023-12-22 15:39:49 -0300

    Connecting to mongo using plugin.

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 import Fastify from "fastify";
 import autoload from "@fastify/autoload";
 import { join } from 'path';
+import { TypeBoxTypeProvider } from "@fastify/type-provider-typebox";
 
 export const build = async (opts: object) => {
-    const app = Fastify(opts);
-
-    app.register(autoload, { dir: join(__dirname, "routes") });
-    
+    const app = Fastify(opts).withTypeProvider<TypeBoxTypeProvider>();
+    await app.register(autoload, { dir: join(__dirname, "plugins") });
+    await app.register(autoload, { dir: join(__dirname, "routes") });
     return app;
 };
--- src/plugins/mongoConnector.ts
+++ src/plugins/mongoConnector.ts
@@ -2,7 +2,7 @@ import fastifyPlugin from "fastify-plugin";
 import fastifyMongodb from "@fastify/mongodb";
 import { FastifyInstance } from "fastify";
 
-export const mongoConnector = fastifyPlugin(async (fastify: FastifyInstance, {}) => {
+export default fastifyPlugin(async (fastify: FastifyInstance, {}) => {
     fastify.register(fastifyMongodb, {
         forceClose: true,
         url: "mongodb://localhost:27017/cmpaas"
