fc51101f24b9448025fdd81f02fd57593655d9f2
Wagner de Andrade Perin <wagnerperin@gmail.com>
2024-01-31 13:47:17 -0300

    Implementing eslint and adjusting code.

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
-import Fastify from "fastify";
-import autoload from "@fastify/autoload";
-import { join } from 'path';
-import { TypeBoxTypeProvider } from "@fastify/type-provider-typebox";
+import Fastify from 'fastify'
+import autoload from '@fastify/autoload'
+import { join } from 'path'
+import { type TypeBoxTypeProvider } from '@fastify/type-provider-typebox'
 
-export const build = async (opts: object) => {
-    const app = Fastify(opts).withTypeProvider<TypeBoxTypeProvider>();
-    await app.register(autoload, { dir: join(__dirname, "plugins") });
-    await app.register(autoload, { dir: join(__dirname, "routes") });
-    return app;
-};
+export const build = async (opts: object): Promise<any> => {
+    const app = Fastify(opts).withTypeProvider< TypeBoxTypeProvider >()
+    await app.register(autoload, { dir: join(__dirname, 'plugins') })
+    await app.register(autoload, { dir: join(__dirname, 'routes') })
+    return app
+}
--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,25 @@
-import { FastifyInstance, FastifyListenOptions } from 'fastify';
-import { build } from './app';
-import dotenv from 'dotenv';
-import closeWithGrace from 'close-with-grace';
+import { type FastifyInstance, type FastifyListenOptions } from 'fastify'
+import { build } from './app'
+import dotenv from 'dotenv'
+import closeWithGrace from 'close-with-grace'
+import path from 'path'
 
-dotenv.config({ path: __dirname + '/.env' });
+dotenv.config({ path: path.join(__dirname, '.env') })
 
-const start = async () => {
-    const app: FastifyInstance = await build({ logger: { transport: { target: 'pino-pretty' }}});
-    app.log.info(`Starting server on ${ process.env.HOST }:${ process.env.PORT }`);
+const start = async (): Promise<void> => {
+    const app: FastifyInstance = await build({ logger: { transport: { target: 'pino-pretty' } } })
+    app.log.info(`Starting server on ${process.env.HOST}:${process.env.PORT}`)
     const options: FastifyListenOptions = {
-        port: Number( process.env.PORT ) || 3000,
-        host: process.env.HOST || '127.0.0.1'
-    };
-    
-    await app.listen( options );
-    closeWithGrace( async ( { signal, err, manual } ) => {
-        if( err ) {
-            app.log.error( `Server closing with error ${ err }` );
-        }
-        await app.close();
-    });
-};
+        port: isNaN(Number(process.env.PORT)) || Number(process.env.PORT) === 0 ? 3000 : Number(process.env.PORT),
+        host: process.env.HOST ?? '127.0.0.1'
+    }
 
-start();
+    await app.listen(options)
+    closeWithGrace(async ({ signal, err, manual }) => {
+        if (err !== null) {
+            app.log.error(`Server closing with error ${err?.message}`)
+        }
+        await app.close()
+    })
+}
+void start()
