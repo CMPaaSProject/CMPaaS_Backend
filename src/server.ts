7e9bdbb47e17ac1f0b25e7a3b18c3d1753c5ab59
Wagner de Andrade Perin <wagnerperin@gmail.com>
2024-01-31 12:34:08 -0300

    Using .env file.

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,23 @@
-import { FastifyInstance } from 'fastify';
+import { FastifyInstance, FastifyListenOptions } from 'fastify';
 import { build } from './app';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: __dirname + '/.env' });
 
 const start = async () => {
+   
     const app: FastifyInstance = await build({ logger: true });
+    app.log.info(`Starting server on ${process.env.HOST}:${process.env.PORT}`);
+    const options: FastifyListenOptions = {
+        port: Number(process.env.PORT) || 3000,
+        host: process.env.HOST || '127.0.0.1'
+    };
     try {
-        await app.listen({ port: 3000, host: "0.0.0.0" });
+        await app.listen(options);
     } catch (err) {
         app.log.error(err);
         process.exit(1);
     }
+
 };
 start();
