3e609ebbe219dcf5cd9525c1911c34ab1a71c4fd
Wagner de Andrade Perin <wagnerperin@gmail.com>
2024-01-31 12:56:12 -0300

    Configuração do logger usando pino-pretty and transport

--- src/server.ts
+++ src/server.ts
@@ -5,19 +5,17 @@ import dotenv from 'dotenv';
 dotenv.config({ path: __dirname + '/.env' });
 
 const start = async () => {
-   
-    const app: FastifyInstance = await build({ logger: true });
-    app.log.info(`Starting server on ${process.env.HOST}:${process.env.PORT}`);
+    const app: FastifyInstance = await build({ logger: { transport: { target: 'pino-pretty' }}});
+    app.log.info(`Starting server on ${ process.env.HOST }:${ process.env.PORT }`);
     const options: FastifyListenOptions = {
-        port: Number(process.env.PORT) || 3000,
+        port: Number( process.env.PORT ) || 3000,
         host: process.env.HOST || '127.0.0.1'
     };
     try {
-        await app.listen(options);
-    } catch (err) {
-        app.log.error(err);
-        process.exit(1);
+        await app.listen( options );
+    } catch( err ) {
+        app.log.error( err );
+        process.exit( 1 );
     }
-
 };
 start();
